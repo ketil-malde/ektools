5cd86f73a372ea19b4ad36c88d048a231cdc4d2f
Ketil Malde <ketil@malde.org>
2021-03-18 09:32:14 +0100

    Don't throw exception when out of input

--- rawdecode.py
+++ rawdecode.py
@@ -8,9 +8,14 @@ def unpickle_iter():
         while True:
              yield pickle.load(sys.stdin.buffer)
     except EOFError:
-        raise StopIteration
+        # raise StopIteration
+        return
 
 for r in unpickle_iter():
     for dt,fs in r.items():
-        print(dt, fs.keys())
+        print(dt,fs.keys())
+        for f in fs:
+            # print(f, fs[f]['mru'])
+            print(f)
+
 
