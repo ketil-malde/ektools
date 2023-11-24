6325b007ac12474e62e6f22afb2d469a93a38cdc
Ketil Malde <ketil@malde.org>
2023-11-24 09:41:27 +0100

    Fix setup for PyPI submission

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(name='ektools',
       version='0.1',
       description='Utilities to read and process Simrad RAW files',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       url='http://github.com/ketil-malde/ektools',
       author='Ketil Malde',
       author_email='ketil@malde.org',
