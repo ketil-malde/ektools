212a429d66c416a892954a6d0bcd19682e26fa6a
Ketil Malde <ketil@malde.org>
2024-05-08 10:49:25 +0200

    Fix various flake8 warnings

--- ektools/__init__.py
+++ ektools/__init__.py
@@ -6,10 +6,10 @@ import mmap
 #              CLEAR = '\033[0m', YELLOW = '\033[93m', RED = '\033[91m'
 
 def warn(*args, **kwargs):
-    print('\033[93mWarning\033[0m',*args, file=sys.stderr, **kwargs)
+    print('\033[93mWarning\033[0m', *args, file=sys.stderr, **kwargs)
 
 def error(*args, **kwargs):
-    print('\033[91mError:\033[0m',*args, file=sys.stderr, **kwargs)
+    print('\033[91mError:\033[0m', *args, file=sys.stderr, **kwargs)
 
 import ektools.simrad_parsers as sp
 import ektools.korona_parsers as kp
@@ -19,7 +19,7 @@ def parse(str):
        Throws an exception if the datagram isn't known.'''
     # duplicates the simrad_raw_file.DGRAM_TYPE_KEY, but with bytestring keys
     parsers = {
-          b'BOT' : sp.SimradBottomParser()
+        b'BOT' : sp.SimradBottomParser()
         , b'CON' : sp.SimradConfigParser()
         , b'DEP' : sp.SimradDepthParser()
         , b'FIL' : sp.SimradFILParser()
@@ -37,7 +37,7 @@ def parse(str):
 
     dgram_type = str[:3]
     p = parsers[dgram_type]
-    return(p.from_string(str, len(str)))
+    return p.from_string(str, len(str))
 
 def index(f, maxcount=None):
     '''Build an index of datagrams in a Simrad RAW file.  This is a list of position, type, length, and (unparsed) contents.'''
@@ -45,16 +45,15 @@ def index(f, maxcount=None):
     with open(f, "rb") as fh:
         with mmap.mmap(fh.fileno(), length=0, access=mmap.ACCESS_READ) as mf:
             pos = 0
-                l, m = struct.unpack('<l4s', mf[pos:pos+8])
-                if pos+l > len(mf):
             count = 0
             while pos < len(mf) and (maxcount is None or count < maxcount):
+                l, m = struct.unpack('<l4s', mf[pos:pos + 8])
+                if pos + l > len(mf):
                     raise Exception('Premature EOF, truncated RAW file?')
-                v = struct.unpack('<l', mf[pos+l+4:pos+l+8])
+                v = struct.unpack('<l', mf[pos + l + 4:pos + l + 8])
                 t = m.decode('latin-1')
-                if v[0]!=l: warn(f'Datagram at {pos}: control lenght mismatch ({l} vs {v[0]}) - endianness error or corrupt file?')
-                idx.append((pos,t,l,mf[pos+4:pos+4+l]))
-                pos += l+8
+                if v[0] != l: warn(f'Datagram at {pos}: control lenght mismatch ({l} vs {v[0]}) - endianness error or corrupt file?')
+                idx.append((pos, t, l, mf[pos + 4:pos + 4 + l]))
+                pos += l + 8
                 count += 1
     return idx
-    
