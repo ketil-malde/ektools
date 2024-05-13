19b600541585b024b15d334d6c2e5ccea8166f02
Ketil Malde <ketil@malde.org>
2024-05-13 12:40:01 +0200

    Moved to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(name='ektools',
-      version='0.1',
-      description='Utilities to read and process Simrad RAW files',
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      url='http://github.com/ketil-malde/ektools',
-      author='Ketil Malde',
-      author_email='ketil@malde.org',
-      license='MIT',
-      packages=['ektools'],
-      scripts=['tools/eklist','tools/eksplit','tools/ekplot', 'tools/ekmeta'],
-      install_requires=['pynmea2', 'numpy', 'lxml', 'pytz'],
-      zip_safe=False)
+setup()
